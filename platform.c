f9afd30f60cb1b8081fcd7809f67b6166d5bb1f0
dekanyb47 <dekanyb47@gmail.com>
2026-09-11 20:59:56 +0200

    Implemented vram formatting, rendering to screen

--- platform.c
+++ platform.c
@@ -9,9 +9,36 @@ typedef struct PlatformState {
   SDL_Window *win;
   SDL_Renderer *renderer;
   SDL_Texture *texture;
+  uint32_t *pixel_buf;
 } PlatformState;
 
-PlatformState *init_platform_state() {
+#define VRAM_ADDR 0x2400
+#define VRAM_LEN 0x1c00
+
+// Converts the 8080's 1bpp vram to a 32bpp rgba formatted pixel buffer that can be used by SDL_UpdateTexture
+uint32_t *format_vram(const uint8_t *memory, const uint16_t vram_addr, const uint16_t vram_len) {
+  // get VRAM_LEN byte long 1bit per pixel formatted pixel buffer (256 bits per readline)
+  uint16_t vram_end_addr = vram_addr + vram_len;
+  uint16_t curr_addr = vram_addr;
+  uint32_t *res = malloc(vram_len * 32);
+  uint32_t res_i = 0;
+
+  if (res == NULL) die("malloc");
+
+  while (curr_addr < vram_end_addr) {
+    for (int8_t bitn = 7; bitn >= 0; bitn--) {
+      if ((memory[curr_addr] & (1 << bitn)) != 0) res[res_i] = 0xffffffff;    // if the current bit is set
+      else res[res_i] = 0x000000ff;
+      res_i++;
+    }
+    
+    curr_addr++;
+  }
+
+  return res;
+}
+
+PlatformState *init_platform_state(EmulatorState *es) {
   PlatformState *platform_state = malloc(sizeof(PlatformState));
   if (platform_state == NULL) die("malloc");
 
@@ -21,11 +48,16 @@ PlatformState *init_platform_state() {
     }
 
   SDL_Window *win = SDL_CreateWindow("Intel 8080 Emulator", 0, 0, 256, 224, 0);
-  SDL_Renderer *renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_TARGETTEXTURE);  
-  SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 300, 300);
-
   platform_state->win = win;
-  platform_state->renderer = renderer;
+
+  SDL_Renderer *renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_TARGETTEXTURE);
+  platform_state->renderer = renderer;  
+
+  // TODO: screen rotation (anticlockwise 90 degrees)
+  // TODO: incorrect rendering to screen
+  SDL_Texture *texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_RGBA8888, SDL_TEXTUREACCESS_TARGET, 300, 300);
+  uint32_t *formatted_vram = format_vram(es->memory, VRAM_ADDR, VRAM_LEN);
+  SDL_UpdateTexture(texture, NULL, formatted_vram, 256 * 4);
   platform_state->texture = texture;
 
   return platform_state;
@@ -58,11 +90,9 @@ void poll_sdl_events(MachineState *ms) {
 }
 
 void update_window(PlatformState *ps) {
-  // temporary lines for debugging SDL
-  SDL_SetRenderTarget(ps->renderer, ps->texture);
-  SDL_RenderClear(ps->renderer);
-
-  SDL_SetRenderDrawColor(ps->renderer, 0xff, 0xff, 0xff, 0xff);
+  // temporary lines for testing
+  // SDL_SetRenderTarget(ps->renderer, ps->texture);
+  // SDL_RenderClear(ps->renderer);
 
   SDL_SetRenderTarget(ps->renderer, NULL);
   SDL_RenderCopy(ps->renderer, ps->texture, NULL, NULL);
@@ -79,10 +109,11 @@ void handle_interrupts(EmulatorState *es, MachineState *ms) {
   }
 }
 
+// TODO: move to separate file
 void init_program(char game_filepath[], EmulatorState **es, MachineState **ms, PlatformState **ps) {  
   *es = init_emulator_state(game_filepath);
   *ms = init_machine_state();
-  *ps = init_platform_state();
+  *ps = init_platform_state(*es);
 }
 
 void start_main_loop(EmulatorState *es, MachineState *ms, PlatformState *ps) {
